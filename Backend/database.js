90213360df5e2c9b6bfa2a790bee75ab14aac2ce
Damaris Mariscal <damarisss0110@gmail.com>
2022-12-08 17:47:57 -0600

    bd

--- Backend/database.js
+++ Backend/database.js
@@ -9,5 +9,36 @@ var con = mysql.createConnection({
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-  });
-  
+    // crear db llamada 'mydb'
+    con.query("CREATE DATABASE patoDonald", function(err, result) {
+      if (err) throw err;
+      console.log("Database created")
+    });
+
+    con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "1234",
+      database: "patoDonald"
+    });
+
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 Record Insterted");
+    });
+
+    var sql = "SELECT * FROM customers";
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+
+    // alert("Hola mundo, estos son los results" + result);
+  });  
